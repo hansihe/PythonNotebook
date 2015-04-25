4e3d5b2d3a49edcaba0fb68abb94b3e922584f95
Hansihe <hansihe@hansihe.com>
2015-04-26 00:28:03 +0200

    add support for julia notebooks

--- apply.sh
+++ apply.sh
@@ -1,5 +1,15 @@
 # Installs all the packages I need
 
+echo "deb http://ppa.launchpad.net/staticfloat/juliareleases/ubuntu trusty main" >> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/staticfloat/juliareleases/ubuntu trusty main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3D3D3ACC
+
+apt-get update
+
+apt-get install -y julia
+
+julia -e "Pkg.add(\"IJulia\")"
+
 for PYTHONVER in 2 3 ; do
   PYTHON="python$PYTHONVER"
   PIP="pip$PYTHONVER"
